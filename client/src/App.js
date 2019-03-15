67e25b182a6a04abb684a0ff2d3b7e39d304ca94
masaun <masanoriuno@gmail.com>
2019-03-15 11:53:36 +0100

    Add console.log(instance) to App.js (after zos push(deploy/migrate) and zos create)

--- client/src/App.js
+++ client/src/App.js
@@ -76,6 +76,7 @@ class App extends Component {
               Wallet.abi,
               deployedNetwork && deployedNetwork.address,
             );
+            console.log(instance); // debug
           }
         }
         if (instance || instanceWallet) {
