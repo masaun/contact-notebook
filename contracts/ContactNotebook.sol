3da63596b6583c6c97a01f0670fb3af25c0a06ce
masaun <masanoriuno@gmail.com>
2019-03-16 08:54:50 +0100

    Fix getContract function

--- contracts/ContactNotebook.sol
+++ contracts/ContactNotebook.sol
@@ -45,7 +45,7 @@ contract ContactNotebook {
 	/* 連絡先の個別取得 */ 
 	function getContact(uint256 _id) public view returns (string memory name, address contactAddress) {
 
-		require (_id < contacts.length - 1, "id is less than number of contact list");
+		require (_id < contacts.length, "id is less than number of contact list");
 		
 		Contact memory contact = contacts[_id];
 
